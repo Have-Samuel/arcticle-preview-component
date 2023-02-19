fec274faa6adc71e4aafb4268deedca7b9c6471b
Have Samuel <samiehave9@gmail.com>
2023-02-19 22:35:21 +0300

    Making the buttons interactive

---
+++ script.js
@@ -0,0 +1,6 @@
+const forwardArrow = document.querySelector('#forward-arrow');
+
+forwardArrow.addEventListener('click', () => {
+  forwardArrow.style.display = 'block';
+  console.log('HELLO!');
+});
