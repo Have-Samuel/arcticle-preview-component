cf00b3bc3fb561370dd4aa9f1a06095b2af70825
Have Samuel <samiehave9@gmail.com>
2023-02-20 01:37:05 +0300

    Stylnig the desktop version

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const backwardArrow = document.querySelector('.self');
 const mediaSection = document.querySelector('.social');
 
 forwardArrow.addEventListener('click', () => {
-  console.log('HELLO!!');
   mediaSection.classList.toggle('visible');
 });
 
 backwardArrow.addEventListener('click', () => {
-  console.log('HELLO!!');
   mediaSection.classList.toggle('visible');
 });
